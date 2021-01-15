134157c0d24bcda9933f5e9be37eb5620d45a468
Sergio Latorre <sergio.latorre@tuebingen.mpg.de>
2021-01-15 10:06:12 +0100

    Add option binay and non-binary for getgenotypes

--- vcfpytools.py
+++ vcfpytools.py
@@ -31,17 +31,24 @@ def get_samples(vcf_file):
         if line.startswith('#CHROM'):
             return line.rstrip().split('\t')[9:]
 
-def get_genotypes(vcf_file, samples):
+def get_genotypes(vcf_file, samples, binary = False):
+    '''
+    Just for haploids
+    '''
     from operator import itemgetter
     indices = [get_samples(vcf_file).index(i) for i in samples]
     indices = [i+9 for i in indices]
     for line in get_body(vcf_file):
+        alleles = {'0':line.split('\t')[3], '1':line.split('\t')[4], '.':'N'}
         genotps = itemgetter(*indices) (line.rstrip().split('\t'))
         if len(samples) > 1: # For >= 2 samples
-            yield [genotype[0] for genotype in genotps]
-        else:
-            yield genotps[0] # For 1 sample
-
-
-
+            if binary == False:
+                yield [alleles[genotype[0]] for genotype in genotps]
+            else:
+                yield [genotype[0] for genotype in genotps]
+        else: # For 1 sample
+            if binary == False:
+                yield alleles[genotps[0]]
+            else:
+                yield genotps[0]
 
