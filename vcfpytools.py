467d4f942bc64f4b15dda6e59cec24433d7c27d7
Sergio Latorre <slatorre@ucl.ac.uk>
2023-04-19 10:13:24 +0100

    Update contig lengths

--- vcfpytools.py
+++ vcfpytools.py
@@ -1,6 +1,7 @@
 import gzip
 from operator import itemgetter
 
+
 def parser(vcf_file):
     if vcf_file.endswith('gz'):
         with gzip.open(vcf_file, 'rb') as f:
@@ -19,6 +20,16 @@ def get_header(vcf_file):
         else:
             break
 
+def get_chr_lengths(vcf_file):
+    header = get_header(vcf_file)
+    lengths = {}
+    for line in header:
+        if line.startswith('##contig'):
+            ID = line.split('ID=')[1].split(',')[0]
+            l = line.split('length=')[1].split('>')[0]
+            lengths[ID] = int(l)
+    return(lengths)
+
 def get_body(vcf_file):
     vcf = parser(vcf_file)
     for line in vcf:
@@ -33,34 +44,43 @@ def get_samples(vcf_file):
         if line.startswith('#CHROM'):
             return line.rstrip().split('\t')[9:]
 
-def get_genotypes_hap(vcf_file, samples, binary = False):
+def get_genotypes_hap(vcf_file, samples, binary = False, positions = False, filtered = True):
     '''
     Just for haploids
     '''
     indices = [get_samples(vcf_file).index(i) for i in samples]
     indices = [i+9 for i in indices]
     for line in get_body(vcf_file):
-        alleles = {'0':line.split('\t')[3], '1':line.split('\t')[4], '.':'N'}
+        filt = line.split('\t')[6]
+        if filtered == True and filt != 'PASS':
+            continue
+        chrom = line.split('\t')[0]
+        loc = int(line.split('\t')[1])
+        pos = (chrom, loc)
+        alleles = {'0':line.split('\t')[3], '1':line.split('\t')[4], '.':'.'}
         genotps = itemgetter(*indices) (line.rstrip().split('\t'))
         if len(samples) > 1: # For >= 2 samples
             if binary == False:
-                yield [alleles[genotype[0]] for genotype in genotps]
+                yield {'Position':pos,'Alleles':(alleles['0'],alleles['1']),'Genotypes':[alleles[genotype[0]] for genotype in genotps]}
             else:
-                yield [genotype[0] for genotype in genotps]
+                yield {'Position':pos, 'Genotypes':[genotype[0] for genotype in genotps]}
         else: # For 1 sample
             if binary == False:
-                yield alleles[genotps[0]]
+                yield {'Position':pos,'Alleles':(alleles['0'],alleles['1']),'Genotypes':alleles[genotps[0]]}
             else:
-                yield genotps[0]
+                yield {'Position':pos, 'Genotypes':genotps[0]}
 
-def get_genotypes_dip(vcf_file, samples, binary = False, phase = '/'):
+def get_genotypes_dip(vcf_file, samples, binary = False, phase = '/', filtered = True):
     '''
     By deafult assumes unphased genotypes. Otherwise change phase to '|'
     '''
-    indices = [vcfpytools.get_samples(vcf_file).index(i) for i in samples]
+    indices = [get_samples(vcf_file).index(i) for i in samples]
     indices = [i+9 for i in indices]
-    for line in vcfpytools.get_body(vcf_file):
-        alleles = {'0':line.split('\t')[3], '1':line.split('\t')[4], '.':'N'}
+    for line in get_body(vcf_file):
+        filt = line.split('\t')[6]
+        if filtered == True and filt != 'PASS':
+            continue
+        alleles = {'0':line.split('\t')[3], '1':line.split('\t')[4], '.':'.'}
         if len(alleles['1']) > 1: # Just biallelic SNPs
             continue
         genotps = [i.split(':')[0].split(phase) for i in itemgetter(*indices) (line.rstrip().split('\t'))]
