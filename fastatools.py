f58efcc5255243ac3ccea7c19b1595a032a33d08
Sergio <s.latorre@ucl.ac.uk>
2024-05-18 17:12:18 +0100

    Add Kimura 2 parameter function

--- fastatools.py
+++ fastatools.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from math import log, sqrt
 
 def readfasta(file):
     """Reads a .fasta file and returns a dictionary
@@ -16,7 +17,7 @@ def readfasta(file):
             else:
                 sequence = sequence + line.strip()
             sequences[nameseq] = sequence
-    return(sequences)
+    return sequences
 
 def simulate_seq(length, CG_content = 0.5):
     """Given a certain length and optionally a CG content
@@ -29,5 +30,26 @@ def simulate_seq(length, CG_content = 0.5):
     props = {'C':CG_content/2,'G':CG_content/2,'A':(1 -CG_content)/2,'T':(1 -CG_content)/2}
     seq = list(''.join([i*(int(length*props[i])) for i in nucl]))
     shuffle(seq)
-    return(''.join(seq))
+    return ''.join(seq)
 
+def kimura2parameter(Seq1, Seq2):
+    if len(Seq1) != len(Seq2):
+        print('Sequences need to be with the same length')
+        return None
+    bases = ('A','C','G','T')
+    transitions = {('C','T'),('T','C'),('A','G'),('G','A')}
+    Seq1 = [i.upper() for i in Seq1]
+    Seq2 = [i.upper() for i in Seq2]
+    counts = {'t':0,'v':0}
+    for n1, n2 in zip(Seq1, Seq2):
+        if n1 in bases and n2 in bases:
+            if n1 != n2:
+                if (n1, n2) in transitions:
+                    counts['t'] += 1
+                else:
+                    counts['v'] += 1
+    N = len(Seq1)
+    P = counts['t']/N
+    Q = counts['v']/N
+    d = -0.5 * log((1 - 2 * P - Q) * sqrt(1 - 2 * Q))
+    return d
